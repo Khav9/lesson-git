4099f7facf70a7fc745168d749766bfb4a389104
KHAV.SAROEUN <KHAV.SAROEUN@pnc.lan>
2024-02-05 19:26:00 +0700

    [feat] list category GL-3

---
+++ models/category.model.php
@@ -0,0 +1,8 @@
+<?php
+function getCategories() : array
+{
+    global $connection;
+    $statement = $connection->prepare("select * from categories");
+    $statement->execute();
+    return $statement->fetchAll();
+}
